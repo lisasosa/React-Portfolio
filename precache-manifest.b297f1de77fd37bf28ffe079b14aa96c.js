bc4e60fca3a72256100712a53cbf5d64c6d9da00
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-09-05 03:07:22 -0500

    Updates

---
+++ precache-manifest.b297f1de77fd37bf28ffe079b14aa96c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0834c1edd588bba9dc06dd95ff74ddd1",
+    "url": "/lisasosa/React-Portfolio/portfolio/index.html"
+  },
+  {
+    "revision": "2878cca6353ce379c76a",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "35aee17e79e109deef17",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/css/main.fde46407.chunk.css"
+  },
+  {
+    "revision": "2878cca6353ce379c76a",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/js/2.f4e16cf0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/js/2.f4e16cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35aee17e79e109deef17",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/js/main.562f6e95.chunk.js"
+  },
+  {
+    "revision": "49847cec66e69498c41d",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/js/runtime-main.26bc8c7c.js"
+  },
+  {
+    "revision": "0f9f6eedcfa022b4ef5d8b5f594f2fdf",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/media/burger.0f9f6eed.png"
+  },
+  {
+    "revision": "e6c1ccc55f0fa4903e126e9ec0272566",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/media/dayplanner.e6c1ccc5.png"
+  },
+  {
+    "revision": "cdf9083a5dc64fee4e8ee263e9c7ae93",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/media/newsapp.cdf9083a.png"
+  },
+  {
+    "revision": "9606955dc7bb704f1893f199a073183c",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/media/project2.9606955d.png"
+  },
+  {
+    "revision": "6baded9c1d4d6c4387b675b0a5beb20b",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/media/team.6baded9c.png"
+  },
+  {
+    "revision": "37beb84a844604b9f4a029e97d0fd1a7",
+    "url": "/lisasosa/React-Portfolio/portfolio/static/media/weather.37beb84a.png"
+  }
+]);
