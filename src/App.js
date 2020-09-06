3739f54f7019db0bc0faac8d4730f4f65373ec80
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-09-06 13:26:15 -0500

    updated router to display homepage

--- src/App.js
+++ src/App.js
@@ -1,72 +1,87 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "./App.css";
 
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-
-
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
 class App extends React.Component {
- 
   constructor(props) {
-  super(props);
-  this.state = {
-    title: 'Elizabeth Sosa',
-    headerLinks: [
-      {title: 'Home', path: '/'},
-      {title: 'About', path: '/about'},
-      {title: 'Contac', path: '/contact'}
-    ],
+    super(props);
+    this.state = {
+      title: "Elizabeth Sosa",
+      headerLinks: [
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contac", path: "/contact" },
+      ],
       home: {
-      title: 'My Projects',
-      subtitle: '',
-      text: 'See my projects below'
-    },
-    about: {
-      title: 'About me',
-      subtitle: '',
-      text: 'See my projects below'
-    },
-    contact: {
-      title: 'Contact Me',
-        }
-  }
+        title: "My Projects",
+        subtitle: "",
+        text: "See my projects below",
+      },
+      about: {
+        title: "About me",
+        subtitle: "",
+        text: "See my projects below",
+      },
+      contact: {
+        title: "Contact Me",
+      },
+    };
   }
   render() {
-  return (
-    <Router>
-    <Container className="p-0" fluid={true}>
+    return (
+      <Router basename="/">
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Elizabeth Sosa</Navbar.Brand>
 
-    <Navbar className="border-bottom" bg="transparent" expand="lg">
-    <Navbar.Brand>Elizabeth Sosa</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <HomePage
+                title={this.state.home.title}
+                subTitle={this.state.home.subTitle}
+                text={this.state.home.text}
+              />
+            )}
+          />
+          <Route
+            path="/about"
+            render={() => <AboutPage title={this.state.about.title} />}
+          />
+          <Route
+            path="/contact"
+            render={() => <ContactPage title={this.state.contact.title} />}
+          />
 
-    <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
-    <Navbar.Collapse id="navar-toggle">
-    <Nav className="ml-auto">
-    <Link className="nav-link" to="/">Home</Link>
-    <Link className="nav-link" to="/about">About</Link>
-    <Link className="nav-link" to="/contact">Contact</Link>
-
-    </Nav>
-    </Navbar.Collapse>
-
-    </Navbar>
-    <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text}/>} />
-    <Route path="/about"  render={() => <AboutPage title={this.state.about.title} />} />
-    <Route path="/contact"  render={() => <ContactPage title={this.state.contact.title} />} />
-
-    <Footer/>
-    </Container>
-    </Router>
-    
-  );
-}
+          <Footer />
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
